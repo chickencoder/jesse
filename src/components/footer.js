53440f8e13535f3b0799d4e464f4aad0bd02d9ed
Jesse Sibley <inounomemynameisjesse@gmail.com>
2020-12-31 13:24:23 +0000

    Add mailbrew link

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
     <footer className="text-center text-xs py-16 space-y-2">
       <div>
         <a
-          href="/"
+          href="https://app.mailbrew.com/chickencoder/jessethesibleys-blog-3vGU4dBVQF6f"
           className="transition duration-100 ease-in-out underline text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-100"
         >
           Subscribe via Mailbrew
